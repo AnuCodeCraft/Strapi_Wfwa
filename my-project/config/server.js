1a266f608c23ad1465534170729b32428a8d85f0
Anupriya Jha <anupriya.jha@medzgo.com>
2021-07-12 17:00:20 +0530

    frist commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7ed083bb4516933b26ef70dc61369d3'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0004a71-e623-425f-b9cc-5f6eff2d0043'
+};
